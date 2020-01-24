1efa936f1cc1666caee7fa67cfa64f5641d11193
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-01-24 21:50:09 +0200

    change order of functions

--- STL4_UnorderedContainers.cpp
+++ STL4_UnorderedContainers.cpp
@@ -12,6 +12,29 @@ void Foo(const unordered_map<char, string>& m)
     // cout << m['F'] << endl;
 }
 
+// Fastest search/insert: O(1).
+// Unordered set/multiset: element value cannot be changed.
+// Unordered map/multimap: element key cannot be changed.
+void UnorderedSet()
+{
+    unordered_set<string> myset = { "Red", "Green", "Blue" };
+    unordered_set<string>::const_iterator itr = myset.find("Green"); // O(1)
+
+    if (itr != myset.end())
+        cout << *itr << endl;
+
+    myset.insert("Yellow"); // O(1)
+
+    vector<string> vec = { "Purple", "Pink" };
+    myset.insert(vec.begin(), vec.end());
+
+    // Hash table specific APIs:
+    cout << "Load factor = " << myset.load_factor() << endl;
+    string x = "Red";
+    cout << x << " is in bucket #" << myset.bucket(x) << endl;
+    cout << "Total buckets #" << myset.bucket_count() << endl;
+}
+
 // Search time is O(1) but it can degrade to O(n).
 // unordered_multimap does not have [] operator.
 void UnorderedMap()
@@ -33,31 +56,10 @@ void UnorderedMap()
     Foo(day);
 }
 
-// Fastest search/insert: O(1).
-// Unordered set/multiset: element value cannot be changed.
-// Unordered map/multimap: element key cannot be changed.
-void UnorderedSet()
-{
-    unordered_set<string> myset = { "Red", "Green", "Blue" };
-    unordered_set<string>::const_iterator itr = myset.find("Green"); // O(1)
-    if (itr != myset.end())
-        cout << *itr << endl;
-    myset.insert("Yellow"); // O(1)
-
-    vector<string> vec = { "Purple", "Pink" };
-    myset.insert(vec.begin(), vec.end());
-
-    // Hash table specific APIs:
-    cout << "Load factor = " << myset.load_factor() << endl;
-    string x = "Red";
-    cout << x << " is in bucket #" << myset.bucket(x) << endl;
-    cout << "Total buckets #" << myset.bucket_count() << endl;
-}
-
 int main()
 {
-    UnorderedMap();
     UnorderedSet();
+    UnorderedMap();
 
     vector<int> vec = { 1,2,3,4 };
     int* p = &vec[2]; // p points to 3.
